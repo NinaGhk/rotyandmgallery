5fcd96645a47160ae8940e10564bdd098375a3c9
Nina Guo <ninaguo1305@gmail.com>
2022-09-23 11:11:00 +0800

    messed  with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import CharacterGallery from "./components/CharacterGallery";
+import {useEffect, useState} from "react";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
+
+export default function App() {
+
+
+    const [Characters, setCharacters] =useState([]);
+
+    useEffect(()=> {
+        axios.get("https://rickandmortyapi.com/api/character")
+            .then((response)=>{
+            return response.characters
+        })
+            .then((characters)=>{
+            setCharacters(characters.results)
+        })
+            .catch((error)=>{
+            console.error(error())
+        });
+
+        axios.get("https://rickandmortyapi.com/api" ,{
+
+             params:{location:20}
+    }
+            .then((response)=>{
+            return response.characters
+        })
+            .then( (data)=>{
+            setCharacters(data.results)
+        })
+             .catch( (error)=>{
+            console.error(error())
+        })
+    },[])
+
+         useEffect(()=>{
+             console.log("i was called!")
+         })
+
+
+
+
+
+
+    return (
+        <div className="App">
+            <header/>
+            <CharacterGallery characters={characterResponse.result}/>
+
+        </div>
+    );
+
 }
 
-export default App;
+
