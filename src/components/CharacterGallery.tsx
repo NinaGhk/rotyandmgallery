ef3016ec77bd24fd4df577ea4215b361a0b8622d
Nina Guo <ninaguo1305@gmail.com>
2022-09-23 11:08:58 +0800

    add CharacterGallery.tsx messed with axios

---
+++ src/components/CharacterGallery.tsx
@@ -0,0 +1,39 @@
+
+
+import {useState} from "react";
+import  CharacterCard from "./CharacterCard";
+import './CharacterCard..css';
+import {Character} from "../model/Character";
+
+type CharacterGalleryProps= {
+    characters: Character[];
+}
+
+export default function CharacterGallery(props: CharacterGalleryProps) {
+    const [filterText, setFiltertext] = useState("")
+    const filteredCharacters = props.characters.filter((char:) => char.name.includes(filterText))
+
+    let myValue = 1
+    console.log("i have changed ! MyValue:" + myValue + 1)
+
+}
+
+
+
+   return (
+
+         <div>
+             <input onChange ={(event)=>SetData(event.target.value)}/>
+             <p> Typed in:{filterText}</p>
+             <div className={"cards"}>
+                 {filteredCharacters.characters.map((charcter:Character)=>
+                 <div className={"cards"}>
+                     <CharacterCard charVal={character}/>
+             </div>)}
+         </div>)
+
+
+)
+
+
+
