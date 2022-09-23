c82080c5b4ee17eee23ff47d6982322a3ed14a61
Nina Guo <ninaguo1305@gmail.com>
2022-09-23 11:46:26 +0800

    messed  with axios with changed

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ export default function App() {
             .then((response)=>{
             return response.characters
         })
-            .then((characters)=>{
-            setCharacters(characters.results)
+            .then((data)=>{
+            setCharacters(data.results)
         })
             .catch((error)=>{
             console.error(error())
@@ -41,11 +41,17 @@ export default function App() {
              console.log("i was called!")
          })
 
-
-
-
-
-
+        /*useEffect(()=> {
+            axios.post("https://rickandmortyapi.com/api/character")
+                .then((response)=>{
+                    return response.characters
+                })
+                .then( (data) {
+                console.log(data.result);
+              })
+                .catch(function (error) {
+                  console.log(error);
+        });*/
     return (
         <div className="App">
             <header/>
@@ -55,5 +61,24 @@ export default function App() {
     );
 
 }
+
+
+      export default function getAllInfo() {
+        return axios.get('/api/character"');
+    }
+
+    export default  function getUserPermissions() {
+        return axios.get('"https://rickandmortyapi.com/api/location/2ß');
+    }
+
+  function getUserAccount() {
+    axios.all([getAllInfo(), getUserPermissions()])
+        .then(axios.spread( (getAllInfo(), getUserPermissions()) {
+        }));
+}
+
+
+
+
 
 
